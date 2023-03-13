d0c79cd63e42873140211570ee23c32b8bc13ba2
Omar Cabarcas <cahocyjei@gmail.com>
2023-03-13 11:19:46 -0500

    deleting comments

--- routes/user.ts
+++ routes/user.ts
@@ -4,19 +4,14 @@ import { createUser, getUsers, getUserById, updateUser, deleteUser } from '../co
 const API_PREFIX = "/users";
 const UserRoutes = express.Router();
 
-// Obtener todos los usuarios
 UserRoutes.get(API_PREFIX + '/', getUsers);
 
-// Obtener un usuario por ID
 UserRoutes.get(API_PREFIX + '/:id', getUserById);
 
-// Crear un nuevo usuario
 UserRoutes.post(API_PREFIX + '/', createUser);
 
-// Actualizar un usuario existente
 UserRoutes.put(API_PREFIX + '/:id', updateUser);
 
-// Eliminar un usuario
 UserRoutes.delete(API_PREFIX + '/:id', deleteUser);
 
 export default UserRoutes;
